f7a1272fccd61c0114680c72ba2e787ee4510af7
Сергей Белоусов <s.belousov@aiby.com>
2023-09-11 00:23:59 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# pdf_summarizer
-Summarize PDF by LLM
+# SmartPDF
+
+A simple python library for AI-powered PDF documents processing.
+
+## Requirements
+
+* Linux, Windows, MacOS
+* Python 3.8.+
+* OpenAI API Key
+
+## Install package
+
+```bash
+pip install smart_pdf
+```
+
+## Install the latest version
+
+```bash
+git clone https://github.com/bes-dev/random_face.git
+cd random_face
+pip install -r requirements.txt
+python download_model.py
+pip install .
+```
+
+## Demo
+
+```bash
+python -m smart_pdf.smart_pdf --help
+```
+
+## Example
+
+```python
+from smart_pdf import SmartPDF
+smart_doc = SmartPDF(
+    path=<path_to_pdf>,
+    openai_api_key=<openai_api_key>
+)
+print(smart_doc)
+print(f"Summary: {smart_doc.summary()}")
+print("Press q! to exit")
+while True:
+    request = input("> ")
+    if request == "q!":
+        break
+    print(smart_doc.smart_search(request))
+```
---
+++ setup.py
@@ -0,0 +1,51 @@
+"""
+Copyright 2023 by Sergei Belousov
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+from setuptools import setup, find_packages
+
+readme = open('README.md').read()
+
+VERSION = '2023.09.11.0'
+
+requirements = [
+    'openai==0.27.6',
+    'langchain==0.0.285',
+    'PyPDF2==3.0.1',
+    'tiktoken==0.4.0',
+    'faiss-cpu==1.7.3'
+]
+
+setup(
+    # Metadata
+    name='smart_pdf',
+    version=VERSION,
+    author='Sergei Belousov aka BeS',
+    author_email='sergei.o.belousov@gmail.com',
+    description='SmartPDF: AI powered pdf reader.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+
+    # Package info
+    packages=find_packages(exclude=('*test*',)),
+
+    #
+    zip_safe=True,
+    install_requires=requirements,
+
+    # Classifiers
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
